d5b7e1ccb367b3afb5b4bac2b5a1352c029ecc71
a2301_A <2438012361@qq.com>
2024-12-20 14:59:49 +0800

    重构llms.py，添加NVIDIA LLM支持并更新try_1.py以使用新模型

--- llama-index/llms.py
+++ llama-index/llms.py
@@ -1,5 +1,8 @@
 from llama_index.llms.openai import OpenAI
 from llama_index.llms.huggingface import HuggingFaceLLM
-def deepseek_llm(**kwargs):
-    llm = HuggingFaceLLM(model_name="Qwen/Qwen2.5-1.5B-Instruct",tokenizer_name="Qwen/Qwen2.5-1.5B-Instruct", **kwargs)
+from llama_index.llms.nvidia import NVIDIA
+def nvidia_llm(**kwargs):
+    llm = NVIDIA(model="meta/llama-3.1-405b-instruct",
+                 api_key="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",
+                 **kwargs)
     return llm
--- llama-index/try_1.py
+++ llama-index/try_1.py
@@ -3,10 +3,12 @@ from llama_index.core import VectorStoreIndex
 from llama_index.core.tools import QueryEngineTool
 from llama_index.core.query_engine import RouterQueryEngine
 from llama_index.core.selectors.llm_selectors import LLMSingleSelector
-from llms import deepseek_llm
+from llama_index.core.indices.loading import load_index_from_storage
+from llama_index.core.storage import StorageContext
+from llms import nvidia_llm
 from embedding import get_embedding_model
 from llama_index.core import Settings
-Settings.llm = deepseek_llm()
+Settings.llm = nvidia_llm()
 Settings.embed_model = get_embedding_model()
 Settings.chunk_size = 1024
 def creat_vector_query_engine():
@@ -21,15 +23,24 @@ def creat_vector_query_engine():
     return vector_query_engine_tools
 
 def create_route_query_engine():
-    vector_query_engine_tool = creat_vector_query_engine()
+    vector_query_engine_tool = load_vector_query_tools()
     
     
     query_engine = RouterQueryEngine.from_defaults(selector=LLMSingleSelector.from_defaults(),query_engine_tools=vector_query_engine_tool)
     return query_engine
 
+def load_vector_query_tools():
+    vector_query_engine_tools = []
+    vector_index = load_index_from_storage(
+        StorageContext.from_defaults(persist_dir="./processed_data"))
+    vector_query_engine_tool = QueryEngineTool.from_defaults(
+                                                                 query_engine=vector_index.as_query_engine())
+    vector_query_engine_tools.append(vector_query_engine_tool)
+    return vector_query_engine_tools
+
+        
 
 if __name__ == "__main__":
     query_engine = create_route_query_engine()
-    response = query_engine.query(f"上海大学什么时候建立")
+    response = query_engine.query(f"这篇文章讲了什么?用中文回答我")
     print(response)
-    print("success")
